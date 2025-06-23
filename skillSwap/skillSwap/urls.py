37cf75960968a49592c68849cd7f2348d960387c
Ayushi Singh <ayushisingh8234@gmail.com>
2025-06-23 10:07:41 +0545

    url for profile

--- skillSwap/skillSwap/urls.py
+++ skillSwap/skillSwap/urls.py
@@ -21,6 +21,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index, name='index'),
+    path('myprofile', views.myprofile, name='myprofile' ),
     path('account/', include('accounts.urls')),
 ]
 
